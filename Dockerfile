8ef3794b41e0523913382c6bc4a0bbc5cb744063
HakjunKim <hakjun.kim@cloudmt.co.kr>
2024-01-23 09:57:11 +0900

    update: DB Connection

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,25 @@
+FROM eclipse-temurin:20-jdk-alpine as build
+WORKDIR /workspace/app
+
+ENV DATABASE_URL Dummy
+ENV DATABASE_USERNAME Dummy
+ENV DATABASE_PASSWORD Dummy
+
+COPY mvnw .
+COPY .mvn .mvn
+COPY mvnw.cmd .
+COPY pom.xml .
+COPY src src
+
+RUN apk update && \
+    apk add dos2unix && \
+    find . -type f -exec dos2unix {} \; && \
+    apk del dos2unix && \
+    rm -rf /var/cache/apk/*
+
+RUN ./mvnw clean package
+
 FROM openjdk:20
 WORKDIR /usr/src/myapp
-COPY target/demo-0.0.1-SNAPSHOT.jar /usr/src/myapp
+COPY --from=build /workspace/app/target/demo-0.0.1-SNAPSHOT.jar /usr/src/myapp
 CMD ["java", "-jar", "/usr/src/myapp/demo-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/example/demo/cotroller/HelloController.java
+++ src/main/java/com/example/demo/cotroller/HelloController.java
@@ -2,11 +2,67 @@ package com.example.demo.cotroller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 public class HelloController {
-    @GetMapping ("/")
+    @Value("${database.url}")
+    private String dbUrl;
+
+    @Value("${database.username}")
+    private String dbUsername;
+
+    @Value("${database.password}")
+    private String dbPassword;
+
+    @GetMapping("/")
     public String hello() {
-        return "Hello World";
+        // Get current time
+        LocalDateTime currentTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedTime = currentTime.format(formatter);
+
+        // Get IP address and host name
+        String ipAddress;
+        String hostName;
+        String result;
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            ipAddress = inetAddress.getHostAddress();
+            hostName = inetAddress.getHostName();
+        } catch (UnknownHostException e) {
+            ipAddress = "N/A";
+            hostName = "N/A";
+        }
+
+        // MySQL database connection
+        try {
+            Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            System.out.println("DB Connect Success");
+            result = "DB Connect Success";
+
+            // 추가 작업을 수행할 수 있습니다.
+
+            // 연결을 닫습니다.
+            connection.close();
+        } catch (SQLException e) {
+            // 데이터베이스 연결 오류 처리
+            System.err.println("DB Connect Error: " + e.getMessage());
+            result = "DB Connect Error";
+            e.printStackTrace();
+        }
+
+        // Build response string
+        return "<b>Current time: </b>" + formattedTime + "<b><br>IP Address: </b>" + ipAddress + "<b><br>Host Name: </b>" + hostName + "<b><br>DB Connect: </b>" + result;
+
+
     }
 }
